fabae87551495f7cfaeef148421a0173022ba6fd
Danny Radosevich <deradosevich@gmail.com>
2022-11-03 12:09:03 -0600

    dive gen fixed

--- divegen.py
+++ divegen.py
@@ -4,41 +4,38 @@ import subprocess as sp
 import os
 import re
 
-toOpen = input("Enter the file to read from:\n") #prompt user for input
-readFrom = open(toOpen) #open file
+#toOpen = input("Enter the file to read from:\n") #prompt user for input
+readFrom = open("divers.htm") #open file
 readFrom = readFrom.read() #read in
 readFrom = readFrom.split("\n") #get each individual line
+#file_name = input("Enter name for the file: ")
 lineNum = 1 #control variable
-writeOut = 0 #will be used as the write out file
+writeOut = open("state3a.xml","w") #will be used as the write out file
 for line in readFrom: #go through every line
-    #print(line+" "+str(lineNum))
-    if lineNum == 6: #this is the line with the meet title
-        newFile = line.replace(' ','')+".xml" #remove spaces, add xml extension, to make file name
-        newFile = newFile.replace('/','-') #clean up bad characters for the date
-        #os.system("touch "+newFile)
-        newFile = str(newFile) #not needed at all
-        writeOut = open(newFile,"w")#open file to write out
-        writeOut.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")#header
-        writeOut.write("<divingevent>\n") #denote this is a diving event
-        writeOut.write("<eventtitle>"+newFile[:-4]+"</eventtitle>\n") #use file name to make title
-    elif lineNum > 13 and "</" not in line: #now to do the divers, if at rigth line
+    
+    line = line.lstrip()
+    if len(line) > 0 and line[0].isdigit(): #now to do the divers, if at rigth line
+        
         #print(line)
         #re.sub('\s+', ' ',line).strip()
         #line.replace("\t"," ")
-        for char in line: #for every character in the line
-            if char.isdigit():
-                line = line.replace(char, '') #if it is a number remove it
-        line = line.replace('.','') #remove all periods
         while '  'in line:
             line = line.replace('  ',' ') #get down to single spacing
 
-        print(line) #print out line for verification, not needed
+        #print(line) #print out line for verification, not needed
+        line = line.replace(",","")
         line = line.split(" ") #split out the line to individual words
-        if(len(line)>4): #bounds check
+        
+        if(len(line)>=4): #bounds check
+            line.remove("")
+            print(line)
+            
+            team = line[-2].upper()
+            team = team[:3]
             newDiver = "\t<diver>\n" #xml write out
-            newDiver+= "\t\t<lastname>"+line[2]+"</lastname>\n" #write out last name
-            newDiver+= "\t\t<firstname>"+line[1]+"</firstname>\n" #writeout first name
-            newDiver+="\t\t<team>"+line[3]+"</team>\n" #write out team
+            newDiver+= "\t\t<lastname>"+line[1]+"</lastname>\n" #write out last name
+            newDiver+= "\t\t<firstname>"+line[2]+"</firstname>\n" #writeout first name
+            newDiver+="\t\t<team>"+team+"</team>\n" #write out team
             newDiver+="\t</diver>\n" #end the diver
             writeOut.write(newDiver) #write out to the file
     lineNum+=1 #increment control
